396f0b4f28b8a5251af71fac4223b7a70952a0a3
unknown <m-esser@mail.de>
2025-05-29 10:52:28 +0200

    modify to be compatible with ubuntu

--- setup-airflow-vm.sh
+++ setup-airflow-vm.sh
@@ -1,7 +1,7 @@
 # Install Go
-sudo apt-get update
-sudo apt-get install golang-go
-go --version
+sudo apt get update
+sudo apt get install golang-go
+go version
 # Install Taskfile
 go install github.com/go-task/task/v3/cmd/task@latest
 # Install UV Python Package Manager
